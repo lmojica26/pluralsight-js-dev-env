a8aae092c71789bcae2181155c37e4a0f2a414bc
System Administrator <root@LMs-MacBook-Pro.local>
2018-04-30 16:55:16 -0400

    Almost done

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -6,13 +6,22 @@ const baseUrl = getBaseUrl();
 export function getUsers() {
   return get('users');
 }
-/*function get(url){
-  return fetch(url).then(onSuccess, onError);
+
+export function deleteUser(id) {
+  return del(`users/${id}`);
 }
-*/
 
  function get(url){
   return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+//Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response){
